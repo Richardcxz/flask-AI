425fa5b91cdfb58da237743b548426ec2c89bbe4
Igao2 <igor.dasilvacosta8@gmail.com>
2023-09-04 12:30:53 -0300

    code for post added

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
-
+#codigo do post
+@app.route("/", methods=['POST'])
+def post():
+ #valordopost = request.form['name do campo(tem que ser o name)']
+  print("B")
 if __name__ == '__main__':
   app.run(port=5000)
 
