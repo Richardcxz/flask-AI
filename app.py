65c92fb34d4c5fc27d0b6f6cf265c85438b4c3da
Igao2 <igor.dasilvacosta8@gmail.com>
2023-09-11 08:49:41 -0300

    Amplitude adicionado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request,jsonify
-
+from calculorota import Gera_Problema,pegardestino
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,6 +11,8 @@ def index():
 def post():
     origem = request.form['origen']
     destino = request.form['destinon']
+    Gera_Problema("rotas.txt")
+    pegardestino(origem,destino)
     print(origem,destino)
     return render_template("index.html")
 
@@ -21,6 +23,4 @@ def post():
 if __name__ == "__main__":
     app.run()
 
-def gerarTudo(inicio,fim):
-    print(inicio,fim)
-    #codigos aqui
+
